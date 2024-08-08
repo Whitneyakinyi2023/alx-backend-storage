8b8193058f9da8f680e81edd19b285fcd313fa5d
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-08-08 12:49:04 +0000

    works

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Create table
+-- Requirements below should be adhered to
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
