36fd50047ce6037f785d641fadcc8d5d934bf4f2
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-08-08 19:27:42 +0000

    Fixed olum names

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
--- SQL script that ranks country origins of bands
--- ordered by the number of (non-unique) fans
-SELECT origin, COUNT(*) AS nb_fans
+-- Comment 1
+-- Comment 2
+SELECT origin AS origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
